e20a666e9b0d43da34acc4ed91305604154f46cf
Sofiya Tepikin <me@kvazar.co.il>
2014-03-26 22:52:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Survivors::Application.config.secret_key_base = '41e8f612735a3f006d0801560209fdcb4eeae19036168de0d1d962e34475e34169e3d25e4791d039a8777e4a80ae9b1c880884d02a60a2adc94356d34f841218'
