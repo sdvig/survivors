27cf1e2196b690ad545897f52442fcd03bc6bac3
Sofiya Tepikin <me@kvazar.co.il>
2014-03-29 09:41:43 +0100

    Sets up the S3

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.0.2'
 #gem 'sqlite3'
 gem 'pg'
 gem 'carrierwave'
+gem 'fog'
+gem 'mini_magick'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,19 +2,28 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version(:preview)  { process :resize_to_fit => [128, 128] }
+  version(:full)     { process :resize_to_fit => [1024, 768] }
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
